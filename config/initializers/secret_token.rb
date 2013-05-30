cd3451e4a19707e3b3f5d09fc18054a6d7fdd696
Anastasia Karpovich <nascar.by@gmail.com>
2013-05-31 02:42:03 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog219::Application.config.secret_token = 'e8a052e208fe224731ebb9ed54447168e7c2b20f59de333fe18a8a2878be3ff998ad4bbc4913d77f8496febf7a8a9d4f341e8ab3b780b02acad55a2239fe8ba1'
