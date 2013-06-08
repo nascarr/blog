daaba028535d43a1ee8143f66adad418c40296a8
Anastasia Karpovich <nascar.by@gmail.com>
2013-06-08 23:33:40 +0400

    possibility to delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ActiveRecord::Base
   validates :name, :presence => true
   validates :title, :presence => true
 
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
